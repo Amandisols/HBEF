6ad3e0a923175f7fe635be9238b2e5b8862e62e0
Amanda Pennino <46430121+Amandisols@users.noreply.github.com>
2024-12-09 13:17:53 -0500

    new repo

---
+++ 01-soil-maps.R
@@ -0,0 +1,64 @@
+
+#Author: Amanda Pennino
+#Date: December 2, 2024
+
+library(sf)
+library(terra)
+library(raster)
+
+
+#Input
+# * Latest HPU map: https://doi.org/10.6073/pasta/a000ef8c9274155b5b2b4bf9622ead23 (Accessed 2024-11-13).
+# * Watershed boundaries
+
+#Output
+# * Watershed 3 HPU map
+# * Watershed 3 outline
+# * Watershed 3 outline with 50m buffer
+
+#Workflow
+#01-Soil map creation
+#02-DEM processing (for hydrologic analysis)
+#03-Identify events (precipitation)
+#04-Water levels & wells (extract gw behavior during events)
+#05-Soil descriptions and chemistry
+#06-Chemistry (water)
+
+
+ws <- vect("vectors/hbef_wsheds.shp")
+
+plot(ws)
+
+## crop to watershed of interest, add 100m buffer
+ws3 <- ws[ws$WS == "WS3", ]
+ws3_50 <- buffer(ws3, 50)
+
+plot(ws3)
+plot(ws3_100)
+
+#1- Bh
+#2- Bhs
+#3- Bimodal
+#4- E podzol
+#5- Histosol
+#6- Inceptisol
+#7- O
+#8- Typical
+
+soils <- rast("rasters/knb-lter-hbr.380.2/modelout2024-02-21.tif")
+
+plot(soils)
+
+## verses mask
+ws3_hpu <- crop(soils, ws3_50)
+ws3_hpu <- mask(ws3_hpu, ws3_50)
+
+plot(ws3_hpu)
+plot(ws3, add = TRUE)
+
+
+## save 
+writeRaster(ws3_hpu, filename = 'rasters/hpu_ws3.tif', overwrite = TRUE)
+writeVector(ws3, filename = 'vectors/ws3.shp', overwrite = TRUE)
+writeVector(ws3_50, filename = 'vectors/ws3_50m-buffer.shp', overwrite = TRUE)
+
---
+++ 02-dem-processing.R
@@ -0,0 +1,100 @@
+#Author: Amanda Pennino
+#Date: December 2, 2024
+
+library(sf)
+library(terra)
+library(whitebox)
+whitebox::wbt_init()
+
+## NOTES ##
+#Processing DEM for hydrologic analysis using WhiteBox tools.
+#Only re-run this again if changing watershed outline
+
+#Input
+# * DEM
+# * Watershed boundaries
+
+#Output
+# * Watershed 3 DEM -raw
+# * Filled 
+
+#Workflow
+#01-Soil map creation
+#02-DEM processing (for hydrologic analysis)
+#03-Identify events (precipitation)
+#04-Water levels & wells (extract gw behavior during events & delineate watersheds, topographic metrics)
+#05-Soil descriptions and soil chemistry
+#06-Chemistry (water)
+
+
+
+hb_dem <- rast("~/projects/DSS/HBEF/data/GIS/grids/dem1m.tif")
+#plot(hb_dem)
+
+ws3 <- vect("vectors/ws3.shp")
+ws3_dem <- crop(hb_dem, ws3)
+ws3_dem <- mask(ws3_dem, ws3)
+
+#plot(ws3_dem)
+
+writeRaster(ws3_dem, filename = 'rasters/dem1m_ws3.tif', overwrite = TRUE)
+
+
+wbt_fill_depressions_wang_and_liu(
+  dem = "rasters/dem1m_ws3.tif",
+  output = "rasters/ws3_fill.tif"
+)
+
+
+wbt_feature_preserving_smoothing(
+  dem = "rasters/ws3_fill.tif",
+  output = "rasters/ws3_fillsmooth.tif", 
+  filter = 11, norm_diff = 15, num_iter = 10
+)
+
+
+wbt_breach_depressions_least_cost(
+  dem = "rasters/ws3_fillsmooth.tif",
+  output = "rasters/ws3_fillsmoothbreach.tif",
+  dist = 5
+)
+
+ws3_dem2 <- rast("rasters/ws3_fillsmoothbreach.tif")
+plot(ws3_dem2)
+
+# Flow accumulation rasters 
+## d8-flow accumulation
+wbt_d8_flow_accumulation(
+  "rasters/ws3_fillsmoothbreach.tif",
+  output = "rasters/ws3_d8.tif",
+  out_type = "cells"
+)
+
+## Multiple direction flow accumulation.
+wbt_md_inf_flow_accumulation(
+  "rasters/ws3_fillsmoothbreach.tif",
+  output = "rasters/ws3_mdinf.tif",
+  out_type = "sca"
+)
+
+wbt_md_inf_flow_accumulation(
+  "rasters/ws3_fillsmoothbreach.tif",
+  output = "rasters/ws3_mdinf_log.tif",
+  out_type = "sca",
+  log = TRUE
+)
+
+
+# Create a flow network using multiple (where a stream would likely be)
+## For 1m DEM use flowgrid <5e3, for 3m use <90
+flowgrid <- raster("rasters/ws3_d8.tif")
+flowgrid[flowgrid < 5e2] <- NA
+flowgrid <- flowgrid*0+1 # Set all NAs = 0, all streams = 1
+flownet <- rasterToPolygons(flowgrid, dissolve = TRUE)
+flownet <- st_as_sf(flownet)
+
+plot(flownet)
+
+writeRaster(flowgrid, filename = "rasters/flowgrid_5e2_dem1m.tif", overwrite = TRUE)
+st_write(flownet, "vectors/flownet_5e2_dem1m.shp")
+
