5a026d0837486e966386f2e12d455b886e7e2626
Amanda Pennino <46430121+Amandisols@users.noreply.github.com>
2024-12-11 16:31:56 -0500

    dem hydro enforcing

--- 02-dem-processing.R
+++ 02-dem-processing.R
@@ -4,6 +4,7 @@
 library(sf)
 library(terra)
 library(whitebox)
+library(raster)
 whitebox::wbt_init()
 
 ## NOTES ##
@@ -32,60 +33,83 @@ hb_dem <- rast("~/projects/DSS/HBEF/data/GIS/grids/dem1m.tif")
 #plot(hb_dem)
 
 ws3 <- vect("vectors/ws3.shp")
-ws3_dem <- crop(hb_dem, ws3)
-ws3_dem <- mask(ws3_dem, ws3)
+dem1m_ws3 <- crop(hb_dem, ws3)
+dem1m_ws3 <- mask(dem1m_ws3, ws3)
 
-#plot(ws3_dem)
-writeRaster(ws3_dem, filename = 'rasters/dem1m_ws3.tif', overwrite = TRUE)
+dem5m_ws3 <- aggregate(dem1m_ws3, fact=5, fun="mean")
+
+#writeRaster(dem1m_ws3, filename = 'rasters/dem1m_ws3.tif', overwrite = TRUE)
+#writeRaster(dem5m_ws3, filename = 'rasters/dem5m_ws3.tif', overwrite = TRUE)
 
 
-# Create temp dir for intermediate steps (to save on data storage)x
+# Create temp dir for intermediate steps (to save on data storage)
 dir_temp <- tempdir()
 
 
 wbt_fill_depressions_wang_and_liu(
-  dem = "rasters/dem1m_ws3.tif",
-  output = "rasters/ws3_fill.tif"
+  dem = "rasters/dem5m_ws3.tif",
+  output = paste0(dir_temp, "fill.tif")
 )
 
 
 wbt_feature_preserving_smoothing(
-  dem = "rasters/ws3_fill.tif",
-  output = "rasters/ws3_fillsmooth.tif", 
+  dem = paste0(dir_temp, "fill.tif"),
+  output = paste0(dir_temp, "fill_smooth.tif"), 
   filter = 11, norm_diff = 15, num_iter = 10
 )
 
 
 wbt_breach_depressions_least_cost(
-  dem = "rasters/ws3_fillsmooth.tif",
-  output = "rasters/ws3_fillsmoothbreach.tif",
+  dem = paste0(dir_temp, "fill_smooth.tif"),
+  output = "rasters/dem5m_ws3_hydro.tif",
   dist = 5
 )
 
-ws3_dem2 <- rast("rasters/ws3_fillsmoothbreach.tif")
-plot(ws3_dem2)
+dem_hydro <- rast("rasters/dem5m_ws3_hydro.tif")
 
 # Flow accumulation rasters 
 ## d8-flow accumulation
 wbt_d8_flow_accumulation(
-  "rasters/ws3_fillsmoothbreach.tif",
-  output = "rasters/ws3_d8.tif",
+  input = "rasters/dem5m_ws3_hydro.tif",
+  output = paste0(dir_temp, "5m_d8.tif"),
   out_type = "cells"
 )
 
 ## Multiple direction flow accumulation.
 wbt_md_inf_flow_accumulation(
-  "rasters/ws3_fillsmoothbreach.tif",
-  output = "rasters/ws3_mdinf.tif",
+  "rasters/dem5m_ws3_hydro.tif",
+  output = "rasters/mdinf5m_ws3.tif",
   out_type = "sca"
 )
 
-wbt_md_inf_flow_accumulation(
-  "rasters/ws3_fillsmoothbreach.tif",
-  output = "rasters/ws3_mdinf_log.tif",
-  out_type = "sca",
-  log = TRUE
-)
+
+## Look at difference
+
+flowgrid <- rast(paste0(dir_temp, "1m_d8.tif"))
+flowgrid[flowgrid < 5e2] <- NA
+flowgrid <- flowgrid*0+1 # Set all NAs = 0, all streams = 1
+flownet <- rasterToPolygons(flowgrid, dissolve = TRUE)
+flownet1m <- st_as_sf(flownet)
+
+plot(flowgrid)
+
+flowgrid <- rast(paste0(dir_temp, "5m_d8.tif"))
+flowgrid[flowgrid < 5e2] <- NA
+flowgrid <- flowgrid*0+1 # Set all NAs = 0, all streams = 1
+flownet <- raster::rasterToPolygons(flowgrid, dissolve = TRUE)
+flownet5m <- st_as_sf(flownet)
+
+
+??rasterToPolygons
+
+
+plot(flownet1m)
+plot(flownet5m)
+
+
+
+writeRaster(flowgrid, filename = "rasters/flowgrid_5e2_dem1m.tif", overwrite = TRUE)
+st_write(flownet, "vectors/flownet_5e2_dem1m.shp")
 
 
 # Create a flow network using multiple (where a stream would likely be)
